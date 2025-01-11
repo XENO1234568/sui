61f83798905c1589a2a08798e0bf5d3e7da474a0
XENO1234568 <faycalada7@gmail.com>
2025-01-11 15:14:02 -0500

    Update contract.ts

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -1,4 +1,4 @@
 // Contract configuration
 export const CONTRACT = {
-  address: 'TBA', // Replace this with your contract address
+  address: 'GnMQFuoBffW1JMcT8p2z6mzCq88weWTFb8UYQmbUpump', // Replace this with your contract address
 };
